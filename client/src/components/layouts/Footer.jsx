7c7db15fb2b4043a6875c3a10c9b332f4bb4c016
Ковалёв Богдан Олегович <kovalev-bo@ais-npp.local>
2023-05-25 17:17:56 +0500

    custom dd

--- client/src/components/layouts/Footer.jsx
+++ client/src/components/layouts/Footer.jsx
@@ -1,47 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import { CustomDropDown } from "../UI/dropDownMenu/CustomDropDown.jsx";
 import { ReactComponent as ToVk } from "../../img/svg/linkVk.svg";
 import { ReactComponent as ToTg } from "../../img/svg/linkTg.svg";
 import { ReactComponent as ToEmail } from "../../img/svg/linkEmail.svg";
-import { ReactComponent as Arrow } from "../../img/svg/arrow.svg";
 
 const Footer = () => {
-  const [menuItems, setMenuItems] = useState(false);
   const year = new Date().getFullYear();
   return (
     <footer>
       <div className="footer__wrapper">
         <hr />
         <div className="footer__section">
-          <div className="footer__info">
-            <div
-              className="footer__info_item"
-              onClick={() => setMenuItems(!menuItems)}
-            >
-              <div className="footer__info_title">
-                <div>покупателям</div>
-                <Arrow className={`dropArrow ${menuItems ? "active" : ""}`} />
-              </div>
-              {menuItems && (
-                <div className="footer__info_dropDown">
-                  <Link
-                    className="footer__info_ddItem"
-                    to="delivery"
-                    onClick={() => setMenuItems(false)}
-                  >
-                    delivery
-                  </Link>
-                  <Link
-                    className="footer__info_ddItem"
-                    to="about"
-                    onClick={() => setMenuItems(false)}
-                  >
-                    about
-                  </Link>
-                </div>
-              )}
-            </div>
-          </div>
+          <CustomDropDown
+            title={"покупателям"}
+            content={[
+              { name: "delivery", link: "delivery" },
+              { name: "about", link: "about" },
+            ]}
+          />
         </div>
         <hr />
         <div className="footer__section">
