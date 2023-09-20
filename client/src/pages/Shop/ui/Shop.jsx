ac5a5581fd9d18d7afcfb7d70c9efb0d767d1a9f
unknown <luckyboggy@gmail.com>
2023-09-20 23:05:49 +0500

    filter modal done

--- client/src/pages/Shop/ui/Shop.jsx
+++ client/src/pages/Shop/ui/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import { ProductList } from "widgets/ProductList/ui/ProductList/ProductList.jsx";
 import { Filters } from "widgets/Filters/ui/Filters.jsx";
 import { ReactComponent as Sort } from "shared/assets/img/svg/sort.svg";
+import { ReactComponent as Close } from "shared/assets/img/svg/close.svg";
 import { Context } from "index.js";
 import { observer } from "mobx-react-lite";
 import { fetchTypes, fetchProducts } from "http/productAPI.js";
@@ -11,6 +12,7 @@ import cls from "./Shop.module.scss";
 
 const Shop = observer(() => {
   const [sort, setSort] = useState(false);
+  const [selectedTypesList, setSelectedTypesList] = useState([]);
   const { product } = useContext(Context);
 
   useEffect(() => {
@@ -31,11 +33,23 @@ const Shop = observer(() => {
       product.limit,
       product.page,
       product.sortType.value
-    ).then((data) => {
-      product.setItems(data.rows);
-      product.setTotalCount(data.count);
-      product.setPageCount();
-    });
+    )
+      .then((data) => {
+        product.setItems(data.rows);
+        product.setTotalCount(data.count);
+        product.setPageCount();
+      })
+      .then(() => {
+        let st = [];
+        product.selectedType.forEach((item) => {
+          st.push(
+            product.types.find((type) => {
+              return type.id === item;
+            })
+          );
+        });
+        setSelectedTypesList(st);
+      });
   }, [product.page, product.selectedType, product.sortType.value]);
 
   return (
@@ -47,18 +61,7 @@ const Shop = observer(() => {
       )}
 
       <div className={cls.toolbar}>
-        <div className={cls.selected}>
-          {product.selectedType.map((item) => (
-            <div key={item} className={cls.selectedType}>
-              {`${
-                product.types.find((type) => {
-                  return type.id === item;
-                }).name
-              }`}
-            </div>
-          ))}
-          {product.selectedType.name /* .toLowerCase() */}
-        </div>
+        <div className={cls.selected}></div>
         <div
           className={cls.filter}
           onClick={() => {
@@ -71,6 +74,20 @@ const Shop = observer(() => {
           </div>
         </div>
       </div>
+      {selectedTypesList && (
+        <div className={cls.selectedTypes}>
+          {selectedTypesList.map((item) => (
+            <div
+              className={cls.selectedType}
+              key={item.name}
+              onClick={() => product.deleteFromSelectedType(item.id)}
+            >
+              <Close className={cls.delete} />
+              <div className={cls.typeName}>{item.name}</div>
+            </div>
+          ))}
+        </div>
+      )}
       <ProductList />
       {product.pageCount > 1 && <Pagination />}
     </div>
